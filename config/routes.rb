28b23e8f4467662c3f3eebefae1e24db3d8be4e9
daisuke0606 <akg666dai@gmail.com>
2020-08-05 11:28:06 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'rooms/new'
   devise_for :users
   get 'messages/index'
   root "messages#index"
   resources :users, only: [:edit, :update, :destroy]
+  resources :rooms, only: [:new, :create]
 end
